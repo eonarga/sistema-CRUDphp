bac287e95f674dfbf3635cc5cb16b986c8d1ed69
Assis Policarpo <assisneto013@gmail.com>
2023-03-07 21:38:37 -0400

    correçao do insert

--- cadastrar.php
+++ cadastrar.php
@@ -3,11 +3,11 @@
 require'Conection.php';
 
 $codigo = filter_input(INPUT_POST,'cod_produto');
-$nome = filter_input(INPUT_POST,'nome');
-$preco = filter_input(INPUT_POST,'preco');
+$nome = filter_input(INPUT_POST,'nome_produto');
+$preco = filter_input(INPUT_POST,'preco_produto');
 
-$sql = $pdo->prepare("INSERT INTO produtos (cod_produto, nome, preco) VALUES (:cod_produto, :nome, :preco)");
+$sql = $pdo->prepare("INSERT INTO produtos (cod_produto, nome_produto, preco_produto) VALUES (:cod_produto, :nome_produto, :preco_produto)");
 $sql->bindValue(':cod_produto', $codigo);
-$sql->bindValue(':nome', $nome);
-$sql->bindValue(':preco', $preco);
+$sql->bindValue(':nome_produto', $nome);
+$sql->bindValue(':preco_produto', $preco);
 $sql->execute();
