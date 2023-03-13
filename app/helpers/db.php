41f77b7a16b7ac34733ac930628e11aea142cdcf
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2023-03-13 14:52:53 -0400

    Aplication update

---
+++ app/helpers/db.php
@@ -0,0 +1,20 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'gabriel';
+    $dbPassword = '123456';
+    $dbName = 'crud';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+     if($conexao->connect_errno)
+     {
+         echo "Erro";
+     }
+     else
+     {
+         echo "Conexão efetuada com sucesso";
+     }
+
+?>
+
